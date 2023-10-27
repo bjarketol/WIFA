54d285c62e8017ff7fe18cdd6bb89e71c3cec61c
REM-SOPHIA MOURADI <rem-sophia.mouradi@edf.fr>
2023-10-27 13:58:03 +0200

    updates in code_saturne api for mesh and calculation robustness

--- cs_api/Dynamique/SRC/cs_user_parameters.c
+++ cs_api/Dynamique/SRC/cs_user_parameters.c
@@ -76,33 +76,52 @@ cs_user_model(void)
   cs_wall_functions_t *wf = cs_get_glob_wall_functions();
   wf->iwalfs = CS_WALL_F_S_MONIN_OBUKHOV;
   wf->iwallf = CS_WALL_F_2SCALES_SMOOTH_ROUGH;
-
-  /* Read the meteo file (1) or impose directly the input values to compute it
-  * in Code_Saturne (2) */  
-  cs_glob_atmo_option->meteo_profile = 2;
-
-  /* Inverse LMO length */
-  cs_glob_atmo_option->meteo_dlmo = cs_notebook_parameter_value_by_name("Lmoinv");
-  /* Large scale roughness */
-  cs_glob_atmo_option->meteo_z0 = cs_notebook_parameter_value_by_name("z0");
-  /* Velocity direction */
-  cs_glob_atmo_option->meteo_angle = cs_notebook_parameter_value_by_name("teta");
   
-  /* /\* Elevation for reference velocity *\/ */
-  /* cs_glob_atmo_option->meteo_zref = cs_notebook_parameter_value_by_name("zref"); */
-  /* /\* Friction velocity *\/ */
-  /* cs_glob_atmo_option->meteo_uref = cs_notebook_parameter_value_by_name("ureff"); */
-  /* Ground temperature */ 
-  cs_glob_atmo_option->meteo_t0 = cs_notebook_parameter_value_by_name("t0");
+  if(cs_notebook_parameter_value_by_name("energy")==1) {
+    cs_glob_physical_model_flag[CS_ATMOSPHERIC] = CS_ATMO_DRY;
+  }
 
+  //TODO : check when it's needed. With constant density or dry atmo?
   /* Automatic open boundary conditions
    *   1: meteo mass flow rate is imposed with a constant large scale
    *      pressure gradient
    *   2: same plus velocity profile imposed at ingoing faces
-   */
+   */  
   cs_glob_atmo_option->open_bcs_treatment = 1; //was 0
 
-  /* /\* Add boundary properties for roughness read in tif file *\/ */
+  /* Read the meteo file (1) or LMO profile with following parameters (2) */  
+  cs_glob_atmo_option->meteo_profile = cs_notebook_parameter_value_by_name("meteo_profile");
+  if(cs_glob_atmo_option->meteo_profile==1) {
+    cs_glob_atmo_option->meteo_file_name = "meteo_file";
+    /* Velocity direction at hub height for actuator disk source terms */
+    cs_glob_atmo_option->meteo_angle = cs_notebook_parameter_value_by_name("teta");
+  }
+  else if (cs_glob_atmo_option->meteo_profile==2) {
+    /* Inverse LMO length */
+    cs_glob_atmo_option->meteo_dlmo = cs_notebook_parameter_value_by_name("Lmoinv");
+    /* Large scale roughness */
+    cs_glob_atmo_option->meteo_z0 = cs_notebook_parameter_value_by_name("z0");
+    /* Velocity direction */
+    cs_glob_atmo_option->meteo_angle = cs_notebook_parameter_value_by_name("teta");
+    /* Elevation for reference velocity */
+    cs_glob_atmo_option->meteo_zref = cs_notebook_parameter_value_by_name("zref");
+    /* Friction velocity */
+    cs_glob_atmo_option->meteo_uref = cs_notebook_parameter_value_by_name("ureff");
+    /* Ground temperature */ 
+    cs_glob_atmo_option->meteo_t0 = cs_notebook_parameter_value_by_name("t0");
+  }
+
+  //Needed for to compute the Coriolis force
+  cs_glob_atmo_option->longitude = cs_notebook_parameter_value_by_name("long");
+  cs_glob_atmo_option->latitude = cs_notebook_parameter_value_by_name("lat");
+  
+  /* Post processing options */
+  
+  /* Output ground in the results file*/
+  cs_glob_atmo_option->compute_z_ground = true;
+  
+  /* Added properties at the boundary */  
+  /* /\* To post process roughness read in tif file *\/ */
   /* cs_parameters_add_property("gdalroughness", */
   /*                            1, */
   /*                            CS_MESH_LOCATION_BOUNDARY_FACES); */
@@ -110,7 +129,22 @@ cs_user_model(void)
   /* cs_parameters_add_property("roughness_zone", */
   /*                            1, */
   /*                            CS_MESH_LOCATION_BOUNDARY_FACES); */
+  
+  /* To post-process k and epsilon */  
+  cs_parameters_add_property("tke_transport",
+                             1,CS_MESH_LOCATION_CELLS);
+  cs_parameters_add_property("eps_transport",
+                             1,CS_MESH_LOCATION_CELLS);
 
+  /* To post-process u* and uk */
+  cs_parameters_add_property("boundary_ustar",
+                             1,
+                             CS_MESH_LOCATION_CELLS);
+
+  cs_parameters_add_property("boundary_uk",
+                             1,
+                             CS_MESH_LOCATION_CELLS);  
+  
 }
 
 /*----------------------------------------------------------------------------*/
@@ -155,6 +189,50 @@ cs_user_parameters(cs_domain_t *domain)
   //Uncomment following if global forcing term is applied
   cs_velocity_pressure_param_t *vp_param = cs_get_glob_velocity_pressure_param();
   vp_param->igpust=0;
+
+  /* Warning, meteo file does not overwrite reference values... */
+  cs_fluid_properties_t *phys_pro = cs_get_glob_fluid_properties();
+  cs_real_t rair = phys_pro->r_pg_cnst;
+  /* Reference fluid properties set from meteo values */
+  phys_pro->p0 = cs_glob_atmo_option->meteo_psea;
+  //phys_pro->t0 = cs_glob_atmo_option->meteo_t0; /* ref temp T0 */
+  //phys_pro->ro0 = phys_pro->p0/(rair * cs_glob_atmo_option->meteo_t0); /* ref density T0 */
+
+  
+  //TODO: clarify if thi is really needed
+  /* ischcv is the type of convective scheme:
+     0: second order linear upwind
+     1: centered
+     2: pure upwind gradient in SOLU
+     3: blending SOLU and centered
+     4: NVD/TVD Scheme */
+
+  /* isstpc:
+     0: slope test enabled
+     1: slope test disabled (default)
+     2: continuous limiter ensuring boundedness (beta limiter) enabled */
+  cs_var_cal_opt_t vcopt;
+  int key_cal_opt_id = cs_field_key_id("var_cal_opt");
+
+  //set numerical options of epsilon
+  cs_field_get_key_struct(CS_F_(eps), key_cal_opt_id, &vcopt);  
+  vcopt.ischcv = 1;
+  vcopt.isstpc = 2;
+  cs_field_set_key_struct(CS_F_(eps), key_cal_opt_id, &vcopt);
+  int kccmin = cs_field_key_id("min_scalar");  
+  /* Set the Value for the Sup and Inf of the studied scalar
+   * for the Gamma beta limiter for the temperature */
+  cs_field_set_key_double(CS_F_(eps), kccmin,0.);
+
+  //set numerical options for k
+  cs_field_get_key_struct(CS_F_(k), key_cal_opt_id, &vcopt);
+  vcopt.ischcv = 1;
+  vcopt.isstpc = 2;
+  cs_field_set_key_struct(CS_F_(k), key_cal_opt_id, &vcopt);  
+  /* Set the Value for the Sup and Inf of the studied scalar
+   * for the Gamma beta limiter for the temperature */
+  cs_field_set_key_double(CS_F_(k), kccmin, 0.);
+  
 }
 
 /*----------------------------------------------------------------------------*/
--- cs_api/cs_api.py
+++ cs_api/cs_api.py
@@ -43,12 +43,16 @@ class CS_study:
 
         #farm configuration
         self.wind_energy_system_file = wind_energy_system_file
+        self.turbine_coordinates_xyz = []
         self.hub_height = 100.0
         self.rotor_diameter = 150.0
         self.farm_size = 1000.0
+        self.hub_height = 100.0
         self.wind_origin=270.0
+        self.cp_curve=[]
+        self.ct_curve=[]
 
-    def run_case(self, mesh_nodes=7, mesh_ntasks_per_nodes=2, mesh_wall_time="1-00:00:00", cs_nodes=2, cs_ntasks_per_nodes=48, cs_wall_time_in_minutes=1800):
+    def run_case(self, mesh_nodes=8, mesh_ntasks_per_nodes=1, mesh_wall_time="2-00:00:00", mesh_partition="bm", cs_nodes=2, cs_ntasks_per_nodes=24, cs_wall_time="2-00:00:00", cs_partition="cn", wckey="wckey"):
         """
         Function to run code_saturne
         For the moment based on writing a bash file and executing it
@@ -61,52 +65,45 @@ class CS_study:
         cs_launch_command = self.cs_path +" submit -p setup.xml --case "+self.case_dir+" --notebook-args"
         for k in range(len(self.notebook_arg_names)):
             cs_launch_command = cs_launch_command + " "+self.notebook_arg_names[k]+"="+str(round(self.notebook_arg_values[k],2))
-        cs_launch_command = cs_launch_command + " --id "+ self.result_dir + " --nodes="+str(cs_nodes)+" --partition=cn -J " +  self.result_dir +" --time="+str(cs_wall_time_in_minutes) + " --ntasks-per-node="+str(cs_ntasks_per_nodes) + " --wckey=P11YD:CODE_SATURNE --exclusive --dependency=afterok:$mesh_jobid\n"
+        cs_launch_command = cs_launch_command + " AD_mesh_cell_size="+str(int(self.rotor_diameter/27.0))
+        cs_launch_command = cs_launch_command + " --id "+ self.result_dir + " --nodes="+str(cs_nodes)+" --partition="+cs_partition+" -J " +  self.result_dir +" --time="+cs_wall_time + " --ntasks-per-node="+str(cs_ntasks_per_nodes) + " --wckey="+wckey+" --exclusive --dependency=afterok:$mesh_jobid\n"
 
 
-        salome_launch_command = self.salome_path + " -t python3 generate_salome_mesh.py args:--turbine_diameter="+str(self.rotor_diameter)+",--turbine_height="+str(self.hub_height)+",--wind_origin="+str(self.wind_origin)+",--disk_mesh_size="+str(int(self.rotor_diameter/27.0))+",--domain_size="+str(np.round(self.farm_size*3,0))+",--domain_height=1000,--output_file='MESH/mesh_for_cs_test.med'"
+        salome_launch_command = self.salome_path + " -t python3 generate_salome_mesh.py args:--turbine_diameter="+str(self.rotor_diameter)+",--turbine_height="+str(self.hub_height)+",--wind_origin="+str(self.wind_origin)+",--disk_mesh_size="+str(int(self.rotor_diameter/20.0))+",--domain_size="+str(np.round(self.farm_size*5,0))+",--domain_height=1000,--output_file='MESH/mesh_for_cs_test.med'"
         #============WRITE BASH FILE FOR SLURM================
-        #TODO :  replace with header from user
-        bash_file.write(fr"""#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=00:10:00
-#SBATCH --partition=bm
-#SBATCH --wckey=P11YD:CODE_SATURNE
-#SBATCH --output=cs_windfarm.out.log
-#SBATCH --error=cs_windfarm.err.log
-#SBATCH --job-name=cs_windfarm
-#
-# Run Mesh generation with salome and get its jobid
-mesh_sbatch_output=$(sbatch <<EOF
-#!/bin/bash
-""")
-        #
-        bash_file.write("#SBATCH --nodes="+str(int(mesh_nodes)))
-        bash_file.write("#SBATCH --cpus-per-task="+str(int(mesh_ntasks_per_nodes)))
-        bash_file.write("#SBATCH --time="+mesh_wall_time)
-        #
-        bash_file.write(fr"""#!/bin/bash
-#SBATCH --partition=bm
-#SBATCH --wckey=P11YD:CODE_SATURNE
-#SBATCH --output=cs_mesh.out.log
-#SBATCH --error=cs_mesh.err.log
-#SBATCH --job-name=cs_mesh
-""")
-        bash_file.write(salome_launch_command)
-        bash_file.write("""
-EOF
-)
+        
+        #HEADER 
+        bash_file.write("#!/bin/bash\n"+"#SBATCH --nodes=1\n"+ \
+                        "#SBATCH --cpus-per-task=1\n"+ \
+                        "#SBATCH --time=00:10:00\n"+ \
+                        "#SBATCH --partition="+mesh_partition+"\n"+ \
+                        "#SBATCH --wckey="+wckey+"\n"+ \
+                        "#SBATCH --output=cs_windfarm.out.log\n"+ \
+                        "#SBATCH --error=cs_windfarm.err.log\n"+ \
+                        "#SBATCH --job-name=cs_windfarm\n")
 
-# Extract the job ID from the output of sbatch
-mesh_jobid=${mesh_sbatch_output##* }
+        #MESH COMMAND
+        bash_file.write("#\n"+"# Run Mesh generation with salome and get its jobid\n"+\
+                        "mesh_sbatch_output=$(sbatch <<EOF\n"+\
+                        "#!/bin/bash\n"+\
+                        "#SBATCH --nodes="+str(int(mesh_nodes))+"\n"+\
+                        "#SBATCH --cpus-per-task="+str(int(mesh_ntasks_per_nodes))+"\n"+\
+                        "#SBATCH --time="+mesh_wall_time+"\n"+\
+                        "#SBATCH --partition="+mesh_partition+"\n"+\
+                        "#SBATCH --wckey="+wckey+"\n"+\
+                        "#SBATCH --output=cs_mesh.out.log\n"+\
+                        "#SBATCH --error=cs_mesh.err.log\n"+\
+                        "#SBATCH --job-name=cs_mesh\n"+\
+                        salome_launch_command+"\n" + "EOF\n"+")\n" + \
+                        "# Extract the job ID from the output of sbatch\n" + \
+                        "mesh_jobid=${mesh_sbatch_output##* }\n" + \
+                        "echo 'meshing job' $mesh_jobid\n")
 
-echo "meshing job" $mesh_jobid
-#
-# Run code_saturne after meshing job is finished
-# with notebooks values:
-""")
-        bash_file.write(cs_launch_command)
+        #CS COMMAND
+        bash_file.write("#\n"+"# Run code_saturne after meshing job is finished\n"+\
+                        "# with notebooks values:\n"+\
+                        cs_launch_command)
+        
         bash_file.close()
         #===============================================
 
@@ -152,7 +149,7 @@ echo "meshing job" $mesh_jobid
         """
         self.wind_energy_system_file = wind_energy_system_file
 
-    def windio_to_cs_files(self):
+    def get_windio_data(self):
         #Load the files and store in dictionaries
         with open(self.wind_energy_system_file) as f:
             wind_system_data = yaml.load(f, Loader=Loader)
@@ -167,7 +164,7 @@ echo "meshing job" $mesh_jobid
         #
         #Turbines information
         self.rotor_diameter = turbines_data['rotor_diameter']
-        self.hub_height =turbines_data['hub_height'] #hauteur moyeu
+        self.hub_height =turbines_data['hub_height']
 
         #Turbine coordinates
         turbine_number = len(farm_layout_data['layouts']['initial_layout']['coordinates']['x'])
@@ -185,22 +182,32 @@ echo "meshing job" $mesh_jobid
         farm_xyz[:,0] = rotor_center_xy_coordinates[0,:]
         farm_xyz[:,1] = rotor_center_xy_coordinates[1,:]
         farm_xyz[:,2] = self.hub_height
-        np.savetxt(self.case_dir + sep +'DATA'+sep+'placement_turbines.csv',farm_xyz,delimiter=',')
 
-        self.farm_size = np.sqrt((np.max(farm_xyz[:,0])-np.min(farm_xyz[:,0]))**2+(np.max(farm_xyz[:,1])-np.min(farm_xyz[:,1]))**2)
-
+        self.farm_size = np.sqrt((np.max(farm_xyz[:,0])-np.min(farm_xyz[:,0]))**2+(np.max(farm_xyz[:,1])-np.min(farm_xyz[:,1]))**2) + self.rotor_diameter
+        self.turbine_coordinates_xyz = farm_xyz
+        
         #TODO: dev if multiple turbine modes / physical conditions
         cp_curve= np.column_stack((turbines_data['performance']['Cp_curve']['Cp_wind_speeds'],turbines_data['performance']['Cp_curve']['Cp_values']))
-        np.savetxt(self.case_dir + sep +'DATA'+sep+'cp_table.csv',cp_curve,delimiter=',')
+        self.cp_curve = cp_curve
         #
         ct_curve= np.column_stack((turbines_data['performance']['Ct_curve']['Ct_wind_speeds'],turbines_data['performance']['Ct_curve']['Ct_values']))
-        np.savetxt(self.case_dir + sep +'DATA'+sep+'ct_table.csv',ct_curve,delimiter=',')
+        self.ct_curve = ct_curve
+        
+        #TODO: read from wind resource from windIO
+        
+    def write_cs_input_files(self):
+        np.savetxt(self.case_dir + sep +'DATA'+sep+'cp_table.csv',self.cp_curve,delimiter=',')
+        np.savetxt(self.case_dir + sep +'DATA'+sep+'ct_table.csv',self.ct_curve,delimiter=',')
+        np.savetxt(self.case_dir + sep +'DATA'+sep+'placement_turbines.csv',\
+                   self.turbine_coordinates_xyz,delimiter=',')
 
+        
 if __name__ == "__main__":
     """
     Main function of script
     """
 
+    #=================SET STUDY FROM WINDIO =============
     #CS case directory
     work_dir = '.'
     os.chdir(work_dir)
@@ -212,19 +219,38 @@ if __name__ == "__main__":
     windfarm_study.set_windio("windio_inputs" + sep + "wind_energy_system"+sep + \
                               "IEA37_case_study_3_wind_energy_system.yaml")
 
+    windfarm_study.get_windio_data()
 
-    windfarm_study.windio_to_cs_files()
+    #=================USER MODIFICATIONS===============
+    
+    #Custom modification, e.g. for sensitivity study
+    windfarm_study.wind_origin = 70.0
+    windfarm_study.domain_height = 1000.0    
+    #windfarm_study.rotor_diameter = 120.0
+    #windfarm_study.hub_height = 100
+    #windfarm_study.turbine_coordinates_xyz[:,2] = windfarm_study.hub_height
+    
+    windfarm_study.write_cs_input_files()
 
 
-    #======================USER DEFINED=================
-    windfarm_study.wind_origin = 270.0
-    windfarm_study.domain_height = 1000.0
+    #
+    # #Example 1 : inflow is in cs meteo file
+    # cs_notebook_parameters = {
+    #     'meteo_profile' : 1,  #1 for meteo_file in case_dir/DATA/meteo_example
+    #     'teta': windfarm_study.wind_origin,
+    #     'WT_d' : windfarm_study.rotor_diameter,
+    #     'st_method' : 0, #0 : homogeneous AD
+    #     'isol' : 0, #0 if full farm, i>0 if turbine i in isolation
+    #     'Coriolis' : 1, #1 if coriolis, 0 if not
+    #     'lat' : 55.0, #latitude for coriolis
+    #     'energy' : 1 #1 to solve energy equation, 0 for constant density
+    # }
 
-    #Simulation parameters. TODO: read from wind resource and user
+    #Example 2 : inflow is in MO profile
     cs_notebook_parameters = {
-        'meteo_profile' : 2,  #1 for meteo_file in case_dir/DATA/meteo_example
-        'Lmoinv': 0.0, #(Lmoinv, z0, zref, teta, ureff, t0) arguments used for meteo_profile 2 only
-        'z0': 0.7,
+        'meteo_profile' : 2,
+        'Lmoinv': 0.0,
+        'z0': 0.1,
         'zref': 10,
         'teta': windfarm_study.wind_origin,
         'ureff' : 10.0,
@@ -234,7 +260,7 @@ if __name__ == "__main__":
         'isol' : 0, #0 if full farm, i>0 if turbine i in isolation
         'Coriolis' : 1, #1 if coriolis, 0 if not
         'lat' : 55.0, #latitude for coriolis
-        'dry' : 1 #1 if dry atmosphere (solve energy equation), 0 if constant density
+        'energy' : 1 #1 to solve energy equation, 0 for constant density
     }
     windfarm_study.set_notebook_param_from_dictionary(cs_notebook_parameters)
     #=================================================
@@ -242,7 +268,7 @@ if __name__ == "__main__":
     #Run case
     windfarm_study.set_result_dir("wind_farm_simulation_"+datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     #
-    windfarm_study.run_case(mesh_nodes=8, mesh_wall_time="0-00:10:00", mesh_ntasks_per_nodes=1, cs_nodes=2, cs_ntasks_per_nodes=48, cs_wall_time_in_minutes=40)
+    windfarm_study.run_case(mesh_nodes=8, mesh_wall_time="0-00:10:00", mesh_ntasks_per_nodes=1, mesh_partition="bm", cs_nodes=2, cs_ntasks_per_nodes=24, cs_wall_time="0-00:40:00", cs_partition="cn", wckey="P120Y:EFLOW")
 
     print('Wrote and launched "launch_cs_windfarm.sh". Waiting for job to finish')
     print("After meshing is finished, the mesh will be stored as "+"MESH"+sep+"mesh_for_cs_test.med")
