4894704c102f8ed80ad2edb0128eeb6fc05b9a0f
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2024-01-05 13:24:07 +0100

    foxes: small update, now including runner

--- vnv/uq_validation/run_foxes.py
+++ vnv/uq_validation/run_foxes.py
@@ -17,27 +17,28 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--runs", help="The foxes parameter sets", default=["A", "B", "C"], nargs="+")
     args = parser.parse_args()
 
-    for case in args.cases:
-        for run in args.runs:
+    with foxes.utils.runners.DaskRunner() as runner:
+        for case in args.cases:
+            for run in args.runs:
 
-            ydir = Path(f"{case}/wind_energy_system")
-            yfile = ydir/f"FLOW_UQ_vnv_toy_study_wind_energy_system_foxes_{run}.yaml"
-            odir = Path(f"foxes_results/{case}")
-            ofile1 = odir/f"power_{case}_foxes_{run}.csv"
-            odir.mkdir(parents=True, exist_ok=True)
+                ydir = Path(f"{case}/wind_energy_system")
+                yfile = ydir/f"FLOW_UQ_vnv_toy_study_wind_energy_system_foxes_{run}.yaml"
+                odir = Path(f"foxes_results/{case}")
+                ofile1 = odir/f"power_{case}_foxes_{run}.csv"
+                odir.mkdir(parents=True, exist_ok=True)
 
-            mbook, farm, states, algo, outs = foxes.input.windio.read_case(yfile)
+                mbook, farm, states, algo, outs = foxes.input.windio.read_case(yfile, runner=runner)
 
-            farm_results = algo.calc_farm()
-            sres = farm_results.isel(state=range(100))
-            fres = sres.to_dataframe()[[FV.AMB_REWS, FV.REWS, FV.AMB_TI, FV.TI, FV.AMB_P, FV.P]]
-            print(fres)
-            print(fres.describe())
-            print()
+                farm_results = algo.calc_farm()
+                sres = farm_results
+                fres = sres.to_dataframe()[[FV.AMB_REWS, FV.REWS, FV.AMB_TI, FV.TI, FV.AMB_P, FV.P]]
+                print(fres)
+                print(fres.describe())
+                print()
 
-            for o in outs:
-                o.name = f"{o.name}_{case}_foxes_{run}"
-                print("Running output", o.name)
-                o.create(farm_results=sres, out_dir=odir, 
-                         auto_fnames=lambda fname: f"{o.name}{fname.suffix}")
-     
+                for o in outs:
+                    o.name = f"{o.name}_{case}_foxes_{run}"
+                    print("Running output", o.name)
+                    o.create(farm_results=sres, out_dir=odir, 
+                            auto_fnames=lambda fname: f"{o.name}{fname.suffix}")
+        
