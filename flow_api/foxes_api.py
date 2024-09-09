c148376582100178039d04322487feb0c7835520
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-09-09 11:49:40 +0200

    dev demo

--- flow_api/foxes_api.py
+++ flow_api/foxes_api.py
@@ -1,10 +1,10 @@
 import argparse
 
-def run_foxes(input_yaml):
+def run_foxes(input_yaml, verbosity=1):
     
     from foxes.input.windio import read_windio
 
-    wio_runner = read_windio(input_yaml, verbosity=3)
+    wio_runner = read_windio(input_yaml, verbosity=verbosity)
 
     with wio_runner as runner:
         runner.run()
@@ -13,9 +13,14 @@ def run():
     
     parser = argparse.ArgumentParser()
     parser.add_argument("input_yaml", help="The input yaml file")
+    parser.add_argument(
+        "-v", "--verbosity", 
+        help="The verbosity level, 0 = silent", 
+        type=int, default=1
+    )
     args = parser.parse_args()
     
-    run_foxes(args.input_yaml)
+    run_foxes(args.input_yaml, verbosity=args.verbosity)
     
 if __name__ == "__main__":
     run()
