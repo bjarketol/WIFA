26f85f27038087f833313688cc1a4a23a1ed03da
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-09-10 12:28:55 +0200

    cleanunp

---
+++ examples/validate_data.py
@@ -0,0 +1,3 @@
+from windIO.utils.yml_utils import validate_yaml, load_yaml
+from windIO.utils import plant_schemas_path
+validate_yaml('cases/windio_4turbines/wind_energy_system/system.yaml', plant_schemas_path + 'wind_energy_system.yaml')
