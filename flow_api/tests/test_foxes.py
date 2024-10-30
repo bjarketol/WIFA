8b70f622ee484885eea98f351943be8c4fd23621
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2024-10-30 13:06:16 +0100

    more foxes tests

--- flow_api/tests/test_foxes.py
+++ flow_api/tests/test_foxes.py
@@ -44,6 +44,51 @@ def test_foxes_4wts2():
     foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
     # print(foxes_aep)
     
+def test_foxes_abl():
+
+    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL/wind_energy_system/FLOW_toy_study_wind_energy_system.yaml'
+
+    # validate input
+    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
+
+    # compute AEP (next step is to return a richer set of outputs)
+    output_dir_name = Path('output_test_foxes')
+    output_dir_name.mkdir(parents=True, exist_ok=True)
+    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
+    # print(foxes_aep)
+    
+    
+def test_foxes_abl_stable():
+
+    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/FLOW_toy_study_wind_energy_system.yaml'
+    
+    # validate input
+    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
+
+    # compute AEP (next step is to return a richer set of outputs)
+    output_dir_name = Path('output_test_foxes')
+    output_dir_name.mkdir(parents=True, exist_ok=True)
+    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
+    # print(foxes_aep)
+  
+def test_foxes_profiles():
+
+    yaml_input = test_path / '../../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/system.yaml'
+    
+    # validate input
+    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
+
+    # compute AEP (next step is to return a richer set of outputs)
+    output_dir_name = Path('output_test_foxes')
+    output_dir_name.mkdir(parents=True, exist_ok=True)
+    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
+    # print(foxes_aep)
+    
 if __name__ == "__main__":
     test_foxes_KUL()
     test_foxes_4wts()
+    test_foxes_4wts2()
+    test_foxes_abl()
+    test_foxes_abl_stable()
+    test_foxes_profiles()
+    
