09f09aff861121150503370970f6fdad3eba2afd
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-12-03 09:42:02 +0100

    updating foxes_api for foxes v1.2.3

--- flow_api/foxes_api.py
+++ flow_api/foxes_api.py
@@ -1,34 +1,147 @@
 import argparse
-from foxes.output import FarmResultsEval
+from pathlib import Path
+from windIO.utils.yml_utils import load_yaml
 
-def run_foxes(input_yaml, output_dir=None, ret_aep=True, verbosity=1):
+def run_foxes(
+        input_yaml,
+        input_dir=None, 
+        output_dir=None, 
+        verbosity=1, 
+        **kwargs,
+    ):
+    """
+    Runs foxes based on windio yaml input
     
-    from foxes.input.windio import read_windio
+    Parameters
+    ----------
+    input_yaml: str or dict
+        Path to the input data file, or the input data
+    input_dir: str, optional
+        The input base directory, for cases where 
+        input_yaml is a dict. In such cases it defaults to 
+        cwd, otherwise to the file containing directory
+    output_dir: str, optional
+        The output base directory, defaults to cwd
+    verbosity: int
+        The verbosity level, 0 = silent
+    
+    Returns
+    -------
+    farm_results: xarray.Dataset, optional
+        The farm results
+    point_results: xarray.Dataset, optional
+        The point results, if requested by input_yaml
+    outputs: list of tuple
+        For each output enty, a tuple (dict, results),
+        where results is a list that represents one
+        entry per function call of the corresponding
+        foxes output class
 
-    wio_runner = read_windio(input_yaml, output_dir=output_dir, verbosity=verbosity)
+    """
 
-    with wio_runner as runner:
-        runner.run()
+    from foxes.input.yaml import read_windio, run_dict
     
-        if ret_aep:
-            o = FarmResultsEval(runner.farm_results)
-            aep = o.calc_farm_yield(algo=runner.algo)
-    
-    return aep
+    if isinstance(input_yaml, dict):
+        wio = input_yaml
+        idir = input_dir
+    else:
+        wio = load_yaml(input_yaml)
+        idir = Path(input_yaml).parent
+
+    idict, states, farm, mbook, odir = read_windio(wio, verbosity=verbosity)
+
+    if output_dir is not None:
+        odir = output_dir
+
+    return run_dict(
+        idict,
+        farm=farm,
+        states=states,
+        mbook=mbook,
+        input_dir=idir,
+        output_dir=odir,
+        verbosity=verbosity,
+        **kwargs,
+    )
 
 def run():
-    
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_yaml", help="The input yaml file")
     parser.add_argument(
-        "-v", "--verbosity", 
-        help="The verbosity level, 0 = silent", 
-        type=int, default=1
+        "input_yaml",
+        help="The windio yaml file",
+    )
+    parser.add_argument(
+        "-o", 
+        "--output_dir", 
+        help="The output directory", 
+        default=None,
+    )
+    parser.add_argument(
+        "-e", 
+        "--engine", 
+        help="The engine", 
+        default=None,
+    )
+    parser.add_argument(
+        "-n", 
+        "--n_procs", 
+        help="The number of processes", 
+        default=None, 
+        type=int,
+    )
+    parser.add_argument(
+        "-c",
+        "--chunksize_states",
+        help="The chunk size for states",
+        default=None,
+        type=int,
+    )
+    parser.add_argument(
+        "-C",
+        "--chunksize_points",
+        help="The chunk size for points",
+        default=5000,
+        type=int,
+    )
+    parser.add_argument(
+        "-it", 
+        "--iterative", 
+        help="Use iterative algorithm",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        help="The verbosity level, 0 = silent",
+        type=int,
+        default=1,
     )
     args = parser.parse_args()
-    
-    aep = run_foxes(args.input_yaml, ret_aep=True, verbosity=args.verbosity)
-    print(f"AEP = {aep:.2f} GWh")
+
+    if (
+        args.engine is not None
+        or args.n_procs is not None
+        or args.chunksize_states is not None
+        or args.chunksize_points is not None
+    ):
+        epars = dict(
+            engine_type=args.engine,
+            n_procs=args.n_procs,
+            chunk_size_states=args.chunksize_states,
+            chunk_size_points=args.chunksize_points,
+            verbosity=args.verbosity,
+        )
+    else:
+        epars = None
+
+    run_foxes(
+        input_yaml=args.input_yaml,
+        output_dir=args.output_dir, 
+        engine_pars=epars,
+        verbosity=args.verbosity, 
+    )
+
     
 if __name__ == "__main__":
     run()
