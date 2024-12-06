3833e2dd5e241aaa5e0df4e69e9c2318b1b15070
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-12-06 10:24:36 +0100

    fixing foxes tests

--- flow_api/tests/test_foxes.py
+++ flow_api/tests/test_foxes.py
@@ -6,83 +6,53 @@ from pathlib import Path
 from flow_api.tests import test_path, windIO_path
 
 def test_foxes_KUL():
-
-    yaml_input = test_path / '../../examples/cases/KUL_LES/wind_energy_system/FLOW_UQ_vnv_toy_study_wind_energy_system.yaml'
-
-    # validate input
+    yaml_input = test_path / '../../examples/cases/KUL_LES/wind_energy_system/system.yaml'
     validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    # compute AEP (next step is to return a richer set of outputs)
     output_dir_name = Path('output_test_foxes')
     output_dir_name.mkdir(parents=True, exist_ok=True)
-    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
-    # print(foxes_aep)
+    run_foxes(yaml_input, output_dir=output_dir_name)
 
 def test_foxes_4wts():
-
-    yaml_input = test_path / '../../examples/cases/windio_4turbines/wind_energy_system/system_no_field.yaml'
-
-    # validate input
+    yaml_input = test_path / '../../examples/cases/windio_4turbines/wind_energy_system/system.yaml'
     validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    # compute AEP (next step is to return a richer set of outputs)
     output_dir_name = Path('output_test_foxes')
     output_dir_name.mkdir(parents=True, exist_ok=True)
-    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
-    # print(foxes_aep)
+    run_foxes(yaml_input, output_dir=output_dir_name)
 
 def test_foxes_4wts2():
-
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_2flowcases/wind_energy_system/FLOW_toy_study_wind_energy_system.yaml'
-
-    # validate input
+    yaml_input = test_path / '../../examples/cases/windio_4turbines_2flowcases/wind_energy_system/system.yaml'
     validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    # compute AEP (next step is to return a richer set of outputs)
     output_dir_name = Path('output_test_foxes')
     output_dir_name.mkdir(parents=True, exist_ok=True)
-    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
-    # print(foxes_aep)
+    run_foxes(yaml_input, output_dir=output_dir_name)
     
 def test_foxes_abl():
-
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL/wind_energy_system/FLOW_toy_study_wind_energy_system.yaml'
-
-    # validate input
+    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL/wind_energy_system/system.yaml'
     validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    # compute AEP (next step is to return a richer set of outputs)
     output_dir_name = Path('output_test_foxes')
     output_dir_name.mkdir(parents=True, exist_ok=True)
-    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
-    # print(foxes_aep)
-    
+    run_foxes(yaml_input, output_dir=output_dir_name)
     
 def test_foxes_abl_stable():
-
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/FLOW_toy_study_wind_energy_system.yaml'
-    
-    # validate input
+    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/system.yaml'
     validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    # compute AEP (next step is to return a richer set of outputs)
     output_dir_name = Path('output_test_foxes')
     output_dir_name.mkdir(parents=True, exist_ok=True)
-    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
-    # print(foxes_aep)
+    run_foxes(yaml_input, output_dir=output_dir_name)
   
 def test_foxes_profiles():
-
     yaml_input = test_path / '../../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/system.yaml'
-    
-    # validate input
     validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    # compute AEP (next step is to return a richer set of outputs)
     output_dir_name = Path('output_test_foxes')
     output_dir_name.mkdir(parents=True, exist_ok=True)
-    foxes_aep = run_foxes(yaml_input, output_dir=output_dir_name)
-    # print(foxes_aep)
+    run_foxes(yaml_input, output_dir=output_dir_name)
+
     
 if __name__ == "__main__":
     test_foxes_KUL()
