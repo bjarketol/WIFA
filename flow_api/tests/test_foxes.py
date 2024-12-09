ca7cc329da17c9e4df4a60233366f0b9f19d5b11
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-12-09 09:18:14 +0100

    generalizing foxes tests

--- flow_api/tests/test_foxes.py
+++ flow_api/tests/test_foxes.py
@@ -4,53 +4,37 @@ from pathlib import Path
 
 from flow_api.tests import test_path, windIO_path
 
-def test_foxes_KUL():
-    yaml_input = test_path / '../../examples/cases/KUL_LES/wind_energy_system/system.yaml'
-    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
 
-    output_dir_name = Path('output_test_foxes')
-    output_dir_name.mkdir(parents=True, exist_ok=True)
-    run_foxes(yaml_input, output_dir=output_dir_name)
+def _run_foxes(wes_dir):
+    for yaml_input in wes_dir.glob("system*.yaml"):
+        validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
+        output_dir_name = Path('output_test_foxes')
+        output_dir_name.mkdir(parents=True, exist_ok=True)
+        run_foxes(yaml_input, output_dir=output_dir_name)
+
+def test_foxes_KUL():
+    wes_dir = test_path / '../../examples/cases/KUL_LES/wind_energy_system'
+    _run_foxes(wes_dir)
 
 def test_foxes_4wts():
-    yaml_input = test_path / '../../examples/cases/windio_4turbines/wind_energy_system/system.yaml'
-    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
-
-    output_dir_name = Path('output_test_foxes')
-    output_dir_name.mkdir(parents=True, exist_ok=True)
-    run_foxes(yaml_input, output_dir=output_dir_name)
+    wes_dir = test_path / '../../examples/cases/windio_4turbines/wind_energy_system/'
+    _run_foxes(wes_dir)
 
 def test_foxes_4wts2():
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_2flowcases/wind_energy_system/system.yaml'
-    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
-
-    output_dir_name = Path('output_test_foxes')
-    output_dir_name.mkdir(parents=True, exist_ok=True)
-    run_foxes(yaml_input, output_dir=output_dir_name)
+    wes_dir = test_path / '../../examples/cases/windio_4turbines_2flowcases/wind_energy_system/'
+    _run_foxes(wes_dir)
     
 def test_foxes_abl():
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL/wind_energy_system/system.yaml'
-    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
-
-    output_dir_name = Path('output_test_foxes')
-    output_dir_name.mkdir(parents=True, exist_ok=True)
-    run_foxes(yaml_input, output_dir=output_dir_name)
+    wes_dir = test_path / '../../examples/cases/windio_4turbines_ABL/wind_energy_system/'
+    _run_foxes(wes_dir)
     
 def test_foxes_abl_stable():
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/system.yaml'
-    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
-
-    output_dir_name = Path('output_test_foxes')
-    output_dir_name.mkdir(parents=True, exist_ok=True)
-    run_foxes(yaml_input, output_dir=output_dir_name)
+    wes_dir = test_path / '../../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/'
+    _run_foxes(wes_dir)
   
 def test_foxes_profiles():
-    yaml_input = test_path / '../../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/system.yaml'
-    validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
-
-    output_dir_name = Path('output_test_foxes')
-    output_dir_name.mkdir(parents=True, exist_ok=True)
-    run_foxes(yaml_input, output_dir=output_dir_name)
+    wes_dir = test_path / '../../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/'
+    _run_foxes(wes_dir)
 
     
 if __name__ == "__main__":
