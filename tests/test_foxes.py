6441b45a5450e2a4d84a16ab62b1673d749adb19
Julian Quick <juqu@dtu.dk>
2024-12-10 10:43:18 +0100

    Complete test directory move (originally moved in commit 023783f)

--- tests/test_foxes.py
+++ tests/test_foxes.py
@@ -1,39 +1,41 @@
 from flow_api.foxes_api import run_foxes
 from windIO.utils.yml_utils import validate_yaml
 from pathlib import Path
-
-from flow_api.tests import test_path, windIO_path
-
+from windIO import __path__ as wiop
+import os
+test_path = Path(os.path.dirname(__file__))
+windIO_path = Path(wiop[0])
 
 def _run_foxes(wes_dir):
     for yaml_input in wes_dir.glob("system*.yaml"):
+        print("\nRUNNING FOXES ON", yaml_input, "\n")
         validate_yaml(yaml_input, windIO_path / 'plant/wind_energy_system.yaml')
         output_dir_name = Path('output_test_foxes')
         output_dir_name.mkdir(parents=True, exist_ok=True)
         run_foxes(yaml_input, output_dir=output_dir_name)
 
 def test_foxes_KUL():
-    wes_dir = test_path / '../../examples/cases/KUL_LES/wind_energy_system'
+    wes_dir = test_path / '../examples/cases/KUL_LES/wind_energy_system/'
     _run_foxes(wes_dir)
 
 def test_foxes_4wts():
-    wes_dir = test_path / '../../examples/cases/windio_4turbines/wind_energy_system/'
+    wes_dir = test_path / '../examples/cases/windio_4turbines/wind_energy_system/'
     _run_foxes(wes_dir)
 
 def test_foxes_4wts2():
-    wes_dir = test_path / '../../examples/cases/windio_4turbines_2flowcases/wind_energy_system/'
+    wes_dir = test_path / '../examples/cases/windio_4turbines_2flowcases/wind_energy_system/'
     _run_foxes(wes_dir)
     
 def test_foxes_abl():
-    wes_dir = test_path / '../../examples/cases/windio_4turbines_ABL/wind_energy_system/'
+    wes_dir = test_path / '../examples/cases/windio_4turbines_ABL/wind_energy_system/'
     _run_foxes(wes_dir)
     
 def test_foxes_abl_stable():
-    wes_dir = test_path / '../../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/'
+    wes_dir = test_path / '../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/'
     _run_foxes(wes_dir)
   
 def test_foxes_profiles():
-    wes_dir = test_path / '../../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/'
+    wes_dir = test_path / '../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/'
     _run_foxes(wes_dir)
 
     
@@ -44,4 +46,3 @@ if __name__ == "__main__":
     test_foxes_abl()
     test_foxes_abl_stable()
     test_foxes_profiles()
-    
