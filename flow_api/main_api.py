94967b32df106ece669b6d9f166df71e4c41467e
Julian Quick <juqu@dtu.dk>
2024-12-11 11:18:42 +0100

    simplify workflow

--- flow_api/main_api.py
+++ flow_api/main_api.py
@@ -22,27 +22,7 @@ def run_api(yaml_input):
     model_name = yaml_dat['attributes']['flow_model']['name']
 
     if model_name.lower() == 'pywake':
-
-        yaml_input_no_ext = os.path.splitext(yaml_input)[0]  # Remove the file extension
-        output_dir_name = 'output_pywake' + yaml_input_no_ext.replace(os.sep, '_')  # Replace directory separators
-        #output_dir_name = 'output_' + yaml_input_no_ext.replace(os.sep, '_')  # Replace directory separators
-        if not os.path.exists(output_dir_name):
-            os.makedirs(output_dir_name)
-
-        # Specify input metadata file name
-        #if 'name' in yaml_dat['attributes']['outputs']:
-        #    output_dir_name = yaml_dat['attributes']['analyses']['outputs']['name']
-        #    if not os.path.exists(output_dir_name):
-        #        os.makedirs(output_dir_name)
-
-        #    output_file = output_dir_name + os.sep + 'recorded_inputs.yaml'
-
-         #   # Write yaml_dat to a YAML file
-         #   with open(output_file, 'w') as outfile:
-         #       yaml.safe_dump(yaml_dat, outfile, default_flow_style=False)
-
-         # compute AEP (next step is to return a richer set of outputs)
-        pywake_aep = run_pywake(yaml_input, output_dir=output_dir_name)
+        pywake_aep = run_pywake(yaml_input)
 
     elif model_name.lower() == 'foxes':
         foxes_aep = run_foxes(yaml_input)
