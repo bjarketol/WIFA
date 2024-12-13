19d109ac60dd6af1b522802aa7f5c3553483d337
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-12-13 11:36:59 +0100

    dev foxes test

--- tests/test_foxes.py
+++ tests/test_foxes.py
@@ -2,65 +2,69 @@ from wifa.foxes_api import run_foxes
 from windIO.utils.yml_utils import validate_yaml
 from pathlib import Path
 from windIO import __path__ as wiop
-import pytest
+from foxes import Engine
+
 import os
 
 test_path = Path(os.path.dirname(__file__))
 windIO_path = Path(wiop[0])
 
 
-def _run_foxes(wes_dir):
-    for yaml_input in wes_dir.glob("system*.yaml"):
-        print("\nRUNNING FOXES ON", yaml_input, "\n")
-        validate_yaml(yaml_input, windIO_path / "plant/wind_energy_system.yaml")
-        output_dir_name = Path("output_test_foxes")
-        output_dir_name.mkdir(parents=True, exist_ok=True)
-        run_foxes(yaml_input, output_dir=output_dir_name)
+class TestFoxes:
 
+    def _run_foxes(self, wes_dir):
+        if not hasattr(self, "_engine"):
+            self._engine = Engine.new("threads", verbosity=0)
+            self._engine.initialize()
+            print("TestFoxes: Using engine", self._engine)
 
-def test_foxes_KUL():
-    wes_dir = test_path / "../examples/cases/KUL_LES/wind_energy_system/"
-    _run_foxes(wes_dir)
+        for yaml_input in wes_dir.glob("system*.yaml"):
+            if "_noXYgrid" not in str(yaml_input):
+                print("\nRUNNING FOXES ON", yaml_input, "\n")
+                validate_yaml(yaml_input, windIO_path / "plant/wind_energy_system.yaml")
+                output_dir_name = Path("output_test_foxes")
+                output_dir_name.mkdir(parents=True, exist_ok=True)
+                try:
+                    run_foxes(yaml_input, output_dir=output_dir_name, engine=None)
+                except Exception as e:
+                    self._engine.finalize()
+                    raise e
 
+    def test_foxes_KUL(self):
+        wes_dir = test_path / "../examples/cases/KUL_LES/wind_energy_system/"
+        self._run_foxes(wes_dir)
 
-@pytest.mark.skip(reason="We need to skip the no xy grid case")
-def test_foxes_4wts():
-    wes_dir = test_path / "../examples/cases/windio_4turbines/wind_energy_system/"
-    _run_foxes(wes_dir)
+    def test_foxes_4wts(self):
+        wes_dir = test_path / "../examples/cases/windio_4turbines/wind_energy_system/"
+        self._run_foxes(wes_dir)
 
+    def test_foxes_abl(self):
+        wes_dir = test_path / "../examples/cases/windio_4turbines_ABL/wind_energy_system/"
+        self._run_foxes(wes_dir)
 
-@pytest.mark.skip(reason="This case was deleted")
-def test_foxes_4wts2():
-    wes_dir = (
-        test_path / "../examples/cases/windio_4turbines_2flowcases/wind_energy_system/"
-    )
-    _run_foxes(wes_dir)
+    def test_foxes_abl_stable(self):
+        wes_dir = (
+            test_path / "../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/"
+        )
+        self._run_foxes(wes_dir)
 
+    def test_foxes_profiles(self):
+        wes_dir = (
+            test_path
+            / "../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/"
+        )
+        self._run_foxes(wes_dir)
 
-def test_foxes_abl():
-    wes_dir = test_path / "../examples/cases/windio_4turbines_ABL/wind_energy_system/"
-    _run_foxes(wes_dir)
-
-
-def test_foxes_abl_stable():
-    wes_dir = (
-        test_path / "../examples/cases/windio_4turbines_ABL_stable/wind_energy_system/"
-    )
-    _run_foxes(wes_dir)
-
-
-def test_foxes_profiles():
-    wes_dir = (
-        test_path
-        / "../examples/cases/windio_4turbines_profiles_stable/wind_energy_system/"
-    )
-    _run_foxes(wes_dir)
+    def __del__(self):
+        if hasattr(self, "_engine"):
+            self._engine.finalize()
 
 
 if __name__ == "__main__":
-    test_foxes_KUL()
-    test_foxes_4wts()
-    test_foxes_4wts2()
-    test_foxes_abl()
-    test_foxes_abl_stable()
-    test_foxes_profiles()
+    runner = TestFoxes()
+    #test_foxes_KUL()
+    runner.test_foxes_4wts()
+    #test_foxes_4wts2()
+    #test_foxes_abl()
+    #test_foxes_abl_stable()
+    #test_foxes_profiles()
